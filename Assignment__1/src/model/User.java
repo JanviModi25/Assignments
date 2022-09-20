3f23f887cc45d2d8e139536db4c02ffa9109120b
JanviModi25 <113518157+JanviModi25@users.noreply.github.com>
2022-09-20 15:25:05 +0530

    Add files via upload

---
+++ Assignment__1/src/model/User.java
@@ -0,0 +1,201 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class User {
+    
+    private String FrName;
+    private String LtName;
+    private String DOB;
+    private String Age;
+    private String TelNum;
+    private String Email;
+    private String StrLn1;
+    private String StrLn2;
+    private String City;
+    private String Country;
+    private String Affiliation;
+    private String Major;
+    private String Career;
+    private String Deg1;
+    private String Deg1Srt;
+    private String Deg1End;
+    private String Deg2;
+    private String Deg2Srt;
+    private String Deg2End;
+    private Image Photo;
+    
+
+    public String getFrName() {
+        return FrName;
+    }
+
+    public void setFrName(String FrName) {
+        this.FrName = FrName;
+    }
+
+    public String getLtName() {
+        return LtName;
+    }
+
+    public void setLtName(String LtName) {
+        this.LtName = LtName;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getTelNum() {
+        return TelNum;
+    }
+
+    public void setTelNum(String TelNum) {
+        this.TelNum = TelNum;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getStrLn1() {
+        return StrLn1;
+    }
+
+    public void setStrLn1(String StrLn1) {
+        this.StrLn1 = StrLn1;
+    }
+
+    public String getStrLn2() {
+        return StrLn2;
+    }
+
+    public void setStrLn2(String StrLn2) {
+        this.StrLn2 = StrLn2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+
+    public String getAffiliation() {
+        return Affiliation;
+    }
+
+    public void setAffiliation(String Affiliation) {
+        this.Affiliation = Affiliation;
+    }
+
+    public String getMajor() {
+        return Major;
+    }
+
+    public void setMajor(String Major) {
+        this.Major = Major;
+    }
+
+    public String getCareer() {
+        return Career;
+    }
+
+    public void setCareer(String Career) {
+        this.Career = Career;
+    }
+
+    public String getDeg1() {
+        return Deg1;
+    }
+
+    public void setDeg1(String Deg1) {
+        this.Deg1 = Deg1;
+    }
+
+    public String getDeg1Srt() {
+        return Deg1Srt;
+    }
+
+    public void setDeg1Srt(String Deg1Srt) {
+        this.Deg1Srt = Deg1Srt;
+    }
+
+    public String getDeg1End() {
+        return Deg1End;
+    }
+
+    public void setDeg1End(String Deg1End) {
+        this.Deg1End = Deg1End;
+    }
+
+    public String getDeg2() {
+        return Deg2;
+    }
+
+    public void setDeg2(String Deg2) {
+        this.Deg2 = Deg2;
+    }
+
+    public String getDeg2Srt() {
+        return Deg2Srt;
+    }
+
+    public void setDeg2Srt(String Deg2Srt) {
+        this.Deg2Srt = Deg2Srt;
+    }
+
+    public String getDeg2End() {
+        return Deg2End;
+    }
+
+    public void setDeg2End(String Deg2End) {
+        this.Deg2End = Deg2End;
+    }
+
+    public Image getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(Image Photo) {
+        this.Photo = Photo;
+    }
+    
+    
+    
+    
+    
+}
